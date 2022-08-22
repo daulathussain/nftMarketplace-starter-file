759b3de27623698b15a81ca49565bad094ebf6da
daulathussain <daulathussain92@gmail.com>
2022-08-22 23:16:12 +0530

    first commit

---
+++ img/index.js
@@ -0,0 +1,89 @@
+import collection from "./collection.png";
+import creatorbackground1 from "./creatorbackground-1.jpeg";
+import creatorbackground2 from "./creatorbackground-2.jpeg";
+import creatorbackground3 from "./creatorbackground-3.jpeg";
+import creatorbackground4 from "./creatorbackground-4.jpg";
+import creatorbackground5 from "./creatorbackground-5.jpg";
+import creatorbackground6 from "./creatorbackground-6.jpg";
+import creatorbackground7 from "./creatorbackground-7.jpg";
+import creatorbackground8 from "./creatorbackground-8.jpg";
+import creatorbackground9 from "./creatorbackground-9.jpg";
+import creatorbackground10 from "./creatorbackground-10.jpg";
+import creatorbackground11 from "./creatorbackground-11.jpg";
+import user1 from "./user-1.png";
+import user2 from "./user-2.png";
+import user3 from "./user-3.png";
+import user4 from "./user-4.png";
+import user5 from "./user-5.png";
+import user6 from "./user-6.png";
+import user7 from "./user-7.png";
+import user8 from "./user-8.png";
+import user9 from "./user-9.png";
+import user10 from "./user-10.png";
+import service1 from "./service-1.png";
+import service2 from "./service-2.png";
+import service3 from "./service-3.png";
+import service4 from "./service-4.png";
+import logoLight from "./logo-light.svg";
+import logo from "./logo.svg";
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import telegram from "./telegram.svg";
+import youtube from "./youtube.svg";
+import vimeo from "./vimeo.svg";
+import vector from "./VectorHIW.svg";
+import zgcmq from "./zgcmq.svg";
+import hero from "./hero.png";
+import nft_1 from "./nft_1.png";
+import getAble from "./getable.png";
+import nft_image_1 from "./nft-image-1.png";
+import nft_image_2 from "./nft-image-2.png";
+import nft_image_3 from "./nft-image-3.png";
+import musiceWave from "./musicwave.png";
+import update from "./update.png";
+
+export default {
+  collection,
+  update,
+  musiceWave,
+  nft_image_1,
+  nft_image_2,
+  nft_image_3,
+  getAble,
+  nft_1,
+  hero,
+  creatorbackground1,
+  creatorbackground2,
+  creatorbackground3,
+  creatorbackground4,
+  creatorbackground5,
+  creatorbackground6,
+  creatorbackground7,
+  creatorbackground8,
+  creatorbackground9,
+  creatorbackground10,
+  creatorbackground11,
+  user1,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  user7,
+  user8,
+  user9,
+  user10,
+  service1,
+  service2,
+  service3,
+  service4,
+  logo,
+  logoLight,
+  facebook,
+  telegram,
+  twitter,
+  vector,
+  vimeo,
+  youtube,
+  zgcmq,
+};
---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+
+const MyApp = ({ Component, pageProps }) => (
+  <div>
+    <Component {...pageProps} />
+  </div>
+);
+
+export default MyApp;
